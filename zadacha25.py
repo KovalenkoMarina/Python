1a5ad68629137e7fc1c13bf2ee0f9fd9bf045ceb
Marina <kovalenko_marina@inbox.ru>
2022-07-25 16:42:27 +0700

    задача 25, укороченная 13

---
+++ zadacha25.py
@@ -0,0 +1,23 @@
+# Задайте список из вещественных чисел. Напишите программу, которая найдёт 
+# разницу между максимальным и минимальным значением дробной части элементов.
+    
+#     *Пример:*
+    
+#     - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+
+list_1 = [1.1, 1.2, 3.1, 5, 10.01]
+list_2 = [round(list_1[i] % 1, 2) for i in range(0, len(list_1)) ]
+
+list_2 = [list_2[j] for j in range(0, len(list_2)) if list_2[j] != 0 ]
+
+max = 0
+min = 1
+for i in range(0, len(list_2)):
+    if list_2[i] > max:
+        max = list_2[i]
+    if list_2[i] < min:
+        min = list_2[i]
+print(max - min)
+        
+
+
