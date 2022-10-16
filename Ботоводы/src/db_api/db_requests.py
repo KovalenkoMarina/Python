dd5938344d209065b249bdf84561cfcf043f083e
Marina <kovalenko_marina@inbox.ru>
2022-10-16 14:07:05 +0700

    'исправленное'

--- Ботоводы/src/db_api/db_requests.py
+++ Ботоводы/src/db_api/db_requests.py
@@ -70,27 +70,27 @@ class Database:
         ])
         return sql, tuple(parameters.values())
 
-    def __init__(self, db_path='food_database.db'):
-        self.db_path = db_path
+    # def __init__(self, db_path='food_database.db'):
+    #     self.db_path = db_path
 
-    @property
-    def connection(self):
-        return sqlite3.connect(self.db_path)
+    # @property
+    # def connection(self):
+    #     return sqlite3.connect(self.db_path)
 
-    def execute(self, sql: str, parameters: tuple = tuple(),
-                fetchone=False, fetchall=False, commit=False):
-        connection = self.connection
-        cursor = connection.cursor()
-        data = None
-        cursor.execute(sql, parameters)
-        if commit:
-            connection.commit()
-        if fetchone:
-            data = cursor.fetchone()
-        if fetchall:
-            data = cursor.fetchall()
-        connection.close()
-        return data
+    # def execute(self, sql: str, parameters: tuple = tuple(),
+    #             fetchone=False, fetchall=False, commit=False):
+    #     connection = self.connection
+    #     cursor = connection.cursor()
+    #     data = None
+    #     cursor.execute(sql, parameters)
+    #     if commit:
+    #         connection.commit()
+    #     if fetchone:
+    #         data = cursor.fetchone()
+    #     if fetchall:
+    #         data = cursor.fetchall()
+    #     connection.close()
+    #     return data
 
     def create_table_foods(self):
         sql = """
@@ -132,9 +132,9 @@ class Database:
     def drop_all(self):
         self.execute("DROP TABLE Foods", commit=True)
 
-    @staticmethod
-    def format_args(sql, parameters: dict) -> tuple:
-        sql += " AND ".join([
-            f"{item} = ?" for item in parameters
-        ])
-        return sql, tuple(parameters.values())
+    # @staticmethod
+    # def format_args(sql, parameters: dict) -> tuple:
+    #     sql += " AND ".join([
+    #         f"{item} = ?" for item in parameters
+    #     ])
+    #     return sql, tuple(parameters.values())
