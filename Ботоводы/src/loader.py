aa692900fdade519fc0c0e8016371aa6873d72e2
Marina <kovalenko_marina@inbox.ru>
2022-10-16 22:24:17 +0700

    'правки'

--- Ботоводы/src/loader.py
+++ Ботоводы/src/loader.py
@@ -17,8 +17,8 @@ except sqlite3.OperationalError as e:
 except Exception as e:
     print(e)
 
-db_path = Path('db_api', 'database', 'food_database.db')
-db = Database(db_path = db_path)
+# db_path = Path('db_api', 'database', 'food_database.db')
+# db = Database(db_path = db_path)
 try:
     db.create_table_foods()
 except sqlite3.OperationalError as e:
